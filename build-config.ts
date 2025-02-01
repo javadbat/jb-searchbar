6298f68c23a18a6b2fa95e6eddfa9d91c16823ad
javadbat <javadbat@gmail.com>
2025-02-01 15:36:34 +0330

    feat: add build config

---
+++ build-config.ts
@@ -0,0 +1,29 @@
+import type { ReactComponentBuildConfig, WebComponentBuildConfig } from "../../tasks/build/builder/src/types.ts";
+
+export const webComponentList: WebComponentBuildConfig[] = [
+  {
+    name: "jb-searchbar",
+    path: "./lib/jb-searchbar.ts",
+    outputPath: "./dist/jb-searchbar.js",
+    external: ["jb-input", "jb-select", "jb-date-input"],
+    umdName: "JBSearchbar",
+    globals: {
+      "jb-input": "JBInput",
+      "jb-select": "JBSelect",
+      "jb-date-input": "JBDateInput",
+    },
+  },
+];
+export const reactComponentList: ReactComponentBuildConfig[] = [
+  {
+    name: "jb-searchbar-react",
+    path: "./react/lib/JBSearchbar.tsx",
+    outputPath: "./react/dist/JBSearchbar.js",
+    external: ["prop-types", "react", "jb-searchbar"],
+    globals: {
+      react: "React",
+      "prop-types": "PropTypes",
+      "jb-searchbar": "JBSearchbar",
+    },
+  },
+];
